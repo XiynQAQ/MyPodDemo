db3d26aadd41d0b751c7cd04aeb43a6dfadf8fe1
DingJia <dingjia-PengP@AP-2.local>
2017-07-12 15:05:07 +0800

    first commit

---
+++ MyPodDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPodDemo
+//
+//  Created by DingJia on 2017/7/12.
+//  Copyright © 2017年 席亚楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPodDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPodDemo
+//
+//  Created by DingJia on 2017/7/12.
+//  Copyright © 2017年 席亚楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
